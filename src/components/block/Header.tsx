d5a868ebb5a247e02ae73f085509bd41989ba966
ebenezeraladesuyi <ebenezeraladesuyi@gmail.com>
2024-03-07 18:16:06 +0100

    new

--- src/components/block/Header.tsx
+++ src/components/block/Header.tsx
@@ -19,9 +19,9 @@ const Header = () => {
 
             {/* <img className="w-[100px] md:w-[130px]" src={logo} alt="W2H-logo" /> */}
 
-            <h4 className='text-[17px] z-50 shadow-md text-black relative p-1 px-3 rounded-md bg-white'>ALAKINKANJU</h4>
+            <h4 className='text-[17px] z-50 shadow-md text-white bg-opacity-40 rounded-[30px] relative p-1 px-3 rounded-m bg-black'>ALAKINKANJU</h4>
 
-            <div>
+            <div className='bg-black bg-opacity-40 rounded-[30px] shadow-md p-3'>
                 <li className="hidden lg:flex items-center gap-5 text-[15px] transition-all duration-500 ease-in-out">
                     <NavLink to="/">
                         <ol className='text-white hover:text-[#80c41c] cursor-pointer font-semibold transition-all duration-300 ease-in-out'>Home</ol>
@@ -66,7 +66,7 @@ const Header = () => {
             dropdown ? */}
 
             <div className={` ${dropdown ? 'transform translate-x-0' : 'transform -translate-y-full'} lg:hidden w-full h-[100vh] absolute bg-whit backdrop-blur-md top-0 transition-all ease-in-out z-30 duration-300`} onClick={showDropdown}>
-                <div className="w-full h-[320px]  flex flex-col pl-[15px] sm:pl-[35px] pt-[25px] text-[13px] gap-4 text-[#1f2035] top-[70px] absolute">
+                <div className="w-full h-[320px]  flex flex-col pl-[15px] sm:pl-[35px] pt-[25px] text-[13px] gap-4 text-[#1f2035] top-[70px] absolute ">
                     {/* <li className="lg:flex items-center gap-5 text-[16px]"> */}
                         <NavLink to="/">
                             <ol className='text-white hover:text-[#80c41c] cursor-pointer font-semibold transition-all duration-300 ease-in-out'>Home</ol>
