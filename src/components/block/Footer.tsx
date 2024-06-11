f8dddd28f9bac55e36daa51c60482e976e3e9bc4
Phinatech <ugbojudithchinemerem@gmail.com>
2024-06-11 17:39:12 +0200

    new changes

--- src/components/block/Footer.tsx
+++ src/components/block/Footer.tsx
@@ -1,52 +1,76 @@
 // import React from 'react'
 
-import { FaSquareFacebook, FaLinkedin, FaSquareInstagram } from "react-icons/fa6";
+import {
+  FaSquareFacebook,
+  FaLinkedin,
+  FaSquareInstagram,
+} from "react-icons/fa6";
 import { FaTwitterSquare } from "react-icons/fa";
 // import { FaLinkedin } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <div  className="w-full h-[60vh bg-black flex flex-col justify-center items-center font-pop">
-        <div className="w-[90%] flex flex-col md:flex-row py-[40px] justify-cente items-cente gap-6">
+    <div className='w-full h-[60vh bg-black flex flex-col justify-center items-center font-pop'>
+      <div className='w-[90%] flex flex-col md:flex-row py-[40px] justify-cente items-cente gap-6'>
+        <div className='flex flex-col gap-6  text-white md:w-[32%] lg:w-[25%]'>
+          <h4 className='font-bold '>Alakikanju</h4>
 
-                <div className="flex flex-col gap-6  text-white md:w-[32%] lg:w-[25%]">
-                    <h4 className="font-bold ">ALAKINKANJU</h4>
+          <h5 className='text-[14px] text-white'>
+            Alakinkanju is a unique digital transformation consultancy &
+            software development company that provides IT solutions.
+          </h5>
 
-                    <h5 className="text-[14px] text-white">
-                        Alakinkanju is  a unique digital transformation consultancy & software development company that provides IT solutions.
-                    </h5>
-
-                    <div className="flex gap-3 items-center text-white text-[20px]">
-                        <div><FaSquareFacebook /></div>
-                        <div><FaTwitterSquare /></div>
-                        <div><FaLinkedin /></div>
-                        <div><FaSquareInstagram /></div>
-                    </div>
-                </div>
-
-                <div className="flex flex-col md:w-[32%] lg:w-[25%] gap-5 text-white">
-                    <h4 className="text-[15px] md:text-[16px] font-bold mb-[1px] text-orange-500">Contacts</h4>
-
-                    <h5 className="text-[13px]"><span className="font-bold">Address:</span> 27, Division Road, Lagos</h5>
-                    <h5 className="text-[13px]"><span className="font-bold">Email:</span> alakinkanju@gmail.com</h5>
-                    <h5 className="text-[13px]"><span className="font-bold">Phone:</span> +234 814 563 7654 </h5>
-                </div>
-
-                <div className="flex flex-col md:w-[32%] lg:w-[25%] text-white gap-3 ">
-                    <h4 className="text-orange-500 text-[15px] font-bold mb-[1px]">Services</h4>
-
-                    <h5 className="text-[14px] ">Product Strategy</h5>
-                    <h5 className="text-[14px]">Product Engineering</h5>
-                    <h5 className="text-[14px]">UI/UX Design</h5>
-                    <h5 className="text-[14px]">Digital Marketing</h5>
-                    <h5 className="text-[14px]">Blockchain</h5>
-                </div>
-
+          <div className='flex gap-3 items-center text-white text-[20px]'>
+            <div>
+              <FaSquareFacebook />
+            </div>
+            <div>
+              <FaTwitterSquare />
+            </div>
+            <div>
+              <FaLinkedin />
+            </div>
+            <div>
+              <FaSquareInstagram />
+            </div>
+          </div>
         </div>
 
-        <h5 className="text-[12px] mt-[0px] mb-[20px] text-white">Copyright &#169; 2024 Alakinkanju</h5>
-    </div>
-  )
-}
+        <div className='flex flex-col md:w-[32%] lg:w-[25%] gap-5 text-white'>
+          <h4 className='text-[15px] md:text-[16px] font-bold mb-[1px] text-orange-500'>
+            Contacts
+          </h4>
 
-export default Footer
+          <h5 className='text-[13px]'>
+            <span className='font-bold'>Address:</span> 3, Oloruntedo street,
+            ipaja Lagos
+          </h5>
+          <h5 className='text-[13px]'>
+            <span className='font-bold'>Email:</span> alakinkanju@gmail.com
+          </h5>
+          <h5 className='text-[13px]'>
+            <span className='font-bold'>Phone:</span> +234 814 563 7654{" "}
+          </h5>
+        </div>
+
+        <div className='flex flex-col md:w-[32%] lg:w-[25%] text-white gap-3 '>
+          <h4 className='text-orange-500 text-[15px] font-bold mb-[1px]'>
+            Services
+          </h4>
+
+          <h5 className='text-[14px] '>Product Strategy</h5>
+          <h5 className='text-[14px]'>Product Engineering</h5>
+          <h5 className='text-[14px]'>UI/UX Design</h5>
+          <h5 className='text-[14px]'>Digital Marketing</h5>
+          <h5 className='text-[14px]'>Blockchain</h5>
+        </div>
+      </div>
+
+      <h5 className='text-[12px] mt-[0px] mb-[20px] text-white'>
+        Copyright &#169; 2024 Alakikanju
+      </h5>
+    </div>
+  );
+};
+
+export default Footer;
--- src/components/block/Header.tsx
+++ src/components/block/Header.tsx
@@ -1,106 +1,127 @@
-import React from 'react'
+import React from "react";
 // import logo from "../../assets/images/logo.png";
 import { HiMenuAlt3 } from "react-icons/hi";
 import { MdClose } from "react-icons/md";
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 import { Link } from "react-scroll";
 
 const Header = () => {
+  const [dropdown, setDropdown] = React.useState(false);
 
-    const [dropdown, setDropdown] = React.useState(false)
-
-    const showDropdown = () => {
-        setDropdown(!dropdown)
-    }
+  const showDropdown = () => {
+    setDropdown(!dropdown);
+  };
 
   return (
-    <div className={`w-full h-[70px] flex justify-center items-center shadow-m font-pop fixed z-40 bg-transparent text-white  `}>
-        <div className="w-[90%] h-full flex justify-between items-center ">
+    <div
+      className={`w-full h-[70px] flex justify-center items-center shadow-m font-pop fixed z-40 bg-transparent text-white  `}
+    >
+      <div className='w-[90%] h-full flex justify-between items-center '>
+        {/* <img className="w-[100px] md:w-[130px]" src={logo} alt="W2H-logo" /> */}
 
-            {/* <img className="w-[100px] md:w-[130px]" src={logo} alt="W2H-logo" /> */}
+        <h4 className='text-[17px] z-50 shadow-md text-white bg-opacity-40 rounded-[30px] relative p-1 px-3 rounded-m bg-black'>
+          Alakikanju
+        </h4>
 
-            <h4 className='text-[17px] z-50 shadow-md text-white bg-opacity-40 rounded-[30px] relative p-1 px-3 rounded-m bg-black'>ALAKINKANJU</h4>
+        <div className='lg:bg-black lg:bg-opacity-40 rounded-[30px] lg:shadow-md p-3'>
+          <li className='hidden lg:flex items-center gap-5 text-[15px] transition-all duration-500 ease-in-out'>
+            <NavLink to='/'>
+              <ol className='text-white hover:text-[#80c41c] cursor-pointer font-semibold transition-all duration-300 ease-in-out'>
+                Home
+              </ol>
+            </NavLink>
 
-            <div className='lg:bg-black lg:bg-opacity-40 rounded-[30px] lg:shadow-md p-3'>
-                <li className="hidden lg:flex items-center gap-5 text-[15px] transition-all duration-500 ease-in-out">
-                    <NavLink to="/">
-                        <ol className='text-white hover:text-[#80c41c] cursor-pointer font-semibold transition-all duration-300 ease-in-out'>Home</ol>
-                    </NavLink>
-
-                    {/* <NavLink to='/'>
+            {/* <NavLink to='/'>
                         <ol className='text-white  hover:text-[#80c41c] cursor-pointer font-semibold transition-all duration-300 ease-in-out'>About</ol>
                     </NavLink> */}
 
-                    <Link offset={-100} smooth={true} duration={500} to="testimonies">
-                        <ol className='text-white  hover:text-[#80c41c] cursor-pointer font-semibold transition-all duration-300 ease-in-out'>Testimonies</ol>
-                    </Link>
+            <Link offset={-100} smooth={true} duration={500} to='testimonies'>
+              <ol className='text-white  hover:text-[#80c41c] cursor-pointer font-semibold transition-all duration-300 ease-in-out'>
+                Testimonies
+              </ol>
+            </Link>
 
-                    {/* <Link offset={-100} smooth={true} duration={500} to="contact"> */}
-                    <NavLink to='/contact'>
-                        <ol className='text-white  hover:text-[#80c41c] cursor-pointer font-semibold transition-all duration-300 ease-in-out'>Contact</ol>
-                    </NavLink>
+            {/* <Link offset={-100} smooth={true} duration={500} to="contact"> */}
+            <NavLink to='/contact'>
+              <ol className='text-white  hover:text-[#80c41c] cursor-pointer font-semibold transition-all duration-300 ease-in-out'>
+                Contact
+              </ol>
+            </NavLink>
 
-                    {/* <NavLink to="/firststep">
+            {/* <NavLink to="/firststep">
                         <button className=' px-4 py-3 bg-[#80c41c] border-none hover:bg-[#3d82b3] text-white font-semibold animate-puls ml-[50px] rounded-[60px] transition-all duration-300 ease-in-out hover:px-5 hover:py-4'>
                             FIRST STEP!
                         </button>
                     </NavLink> */}
-                </li>
+          </li>
 
-                
-
-            { dropdown ?
-                <div className="lg:hidden text-[22px] cursor-pointer  ml-[6px] shadow-md text-black relative z-50 p-1 rounded-md bg-white" onClick={showDropdown}>
-                    <MdClose /> 
-                </div>
-            :
-                <div className="lg:hidden text-[22px] cursor-pointer text-black shadow-md bg-white p-1 rounded-md ml-[6px] relative z-50" onClick={showDropdown}>
-                    <HiMenuAlt3 /> 
-                </div>
-            }
-
+          {dropdown ? (
+            <div
+              className='lg:hidden text-[22px] cursor-pointer  ml-[6px] shadow-md text-black relative z-50 p-1 rounded-md bg-white'
+              onClick={showDropdown}
+            >
+              <MdClose />
             </div>
-
+          ) : (
+            <div
+              className='lg:hidden text-[22px] cursor-pointer text-black shadow-md bg-white p-1 rounded-md ml-[6px] relative z-50'
+              onClick={showDropdown}
+            >
+              <HiMenuAlt3 />
+            </div>
+          )}
         </div>
+      </div>
 
-        {/* {
+      {/* {
             dropdown ? */}
 
-            <div className={` ${dropdown ? 'transform translate-x-0' : 'transform -translate-y-full'} lg:hidden w-full h-[100vh] absolute bg-whit backdrop-blur-md top-0 transition-all ease-in-out z-30 duration-300`} onClick={showDropdown}>
-                <div className="w-full h-[320px]  flex flex-col pl-[15px] sm:pl-[35px] pt-[25px] text-[13px] gap-4 text-[#1f2035] top-[70px] absolute ">
-                    {/* <li className="lg:flex items-center gap-5 text-[16px]"> */}
-                        <NavLink to="/">
-                            <ol className='text-white hover:text-[#80c41c] cursor-pointer font-semibold transition-all duration-300 ease-in-out'>Home</ol>
-                        </NavLink>
+      <div
+        className={` ${
+          dropdown ? "transform translate-x-0" : "transform -translate-y-full"
+        } lg:hidden w-full h-[100vh] absolute bg-whit backdrop-blur-md top-0 transition-all ease-in-out z-30 duration-300`}
+        onClick={showDropdown}
+      >
+        <div className='w-full h-[320px]  flex flex-col pl-[15px] sm:pl-[35px] pt-[25px] text-[13px] gap-4 text-[#1f2035] top-[70px] absolute '>
+          {/* <li className="lg:flex items-center gap-5 text-[16px]"> */}
+          <NavLink to='/'>
+            <ol className='text-white hover:text-[#80c41c] cursor-pointer font-semibold transition-all duration-300 ease-in-out'>
+              Home
+            </ol>
+          </NavLink>
 
-                        {/* <NavLink to='/'>
+          {/* <NavLink to='/'>
                             <ol className='text-white  hover:text-[#80c41c] cursor-pointer font-semibold transition-all duration-300 ease-in-out'>About</ol>
                         </NavLink> */}
 
-                        <Link  offset={-100} smooth={true} duration={500} to="testimonies">
-                            <ol className='text-white  hover:text-[#80c41c] cursor-pointer font-semibold transition-all duration-300 ease-in-out'>Testimonies</ol>
-                        </Link>
+          <Link offset={-100} smooth={true} duration={500} to='testimonies'>
+            <ol className='text-white  hover:text-[#80c41c] cursor-pointer font-semibold transition-all duration-300 ease-in-out'>
+              Testimonies
+            </ol>
+          </Link>
 
-                        {/* <Link offset={-100} smooth={true} duration={500} to="contact"> */}
-                        <NavLink to='/'>
-                            <ol className='text-white  hover:text-[#80c41c] cursor-pointer font-semibold transition-all duration-300 ease-in-out'>Contact</ol>
-                        </NavLink>
+          {/* <Link offset={-100} smooth={true} duration={500} to="contact"> */}
+          <NavLink to='/'>
+            <ol className='text-white  hover:text-[#80c41c] cursor-pointer font-semibold transition-all duration-300 ease-in-out'>
+              Contact
+            </ol>
+          </NavLink>
 
-                        {/* <NavLink to="/firststep">
+          {/* <NavLink to="/firststep">
                             <button className='w-[150px] bg-[#80c41c] border-none hover:bg-[#3d82b3] text-white font-semibold animate-pulse  rounded-[60px]'>
                                 FIRST STEP!
                             </button>
                         </NavLink> */}
-                    {/* </li> */}
-                </div>
-            </div>
+          {/* </li> */}
+        </div>
+      </div>
 
-            {/* :
+      {/* :
                 null
         } */}
     </div>
-  )
-}
+  );
+};
 
 export default Header;
 
